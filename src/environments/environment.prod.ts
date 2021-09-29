8b27be1d1dfb886f51d5c0bdc8d5f1e043884193
Adam Walker <adamwalker101@gmail.com>
2021-09-29 20:59:18 +0100

    [Build] 2.6.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  version: '2.5.0',
+  version: '2.6.1',
   api: {
     serverUrl: 'https://nitr0gen.oa.r.appspot.com',
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: 'dev-2.5.0',
+  version: 'dev-2.6.1',
   api: {
     serverUrl: 'http://localhost:7000',
     //serverUrl: 'https://wallet-dot-notabox.nw.r.appspot.com',
