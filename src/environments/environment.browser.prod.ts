eaa767b1b376f306a394e2f2b2b2d00aa828db16
Adam Walker <adamwalker101@gmail.com>
2021-12-07 12:11:12 +0000

    Package Upgrades

--- src/environments/environment.browser.prod.ts
+++ src/environments/environment.browser.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   browser: true,
-  version: 'Browser-Dev-2.2.0',
+  version: 'Browser-2.2.0',
   api: {
     serverUrl: 'https://nitr0gen.oa.r.appspot.com',
   },
